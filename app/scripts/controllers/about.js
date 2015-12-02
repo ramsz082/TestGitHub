730cb62966d8cb00f5a74ca98690681d5c4ca696
Ramya <ramsz082@gmail.com>
2015-12-02 11:00:51 +0800

    Adding angular app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testGithubApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the testGithubApp
+ */
+angular.module('testGithubApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testGithubApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testGithubApp
+ */
+angular.module('testGithubApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
